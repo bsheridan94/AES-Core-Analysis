3adb6e06058ccf6f830446d8ddf0a9a5861bf4d4
Sheridan <bts37@xunil-03.coe.drexel.edu>
2017-04-12 11:07:29 -0400

    end of class commit

--- dc_script.tcl
+++ dc_script.tcl
@@ -3,14 +3,14 @@ lappend search_path aes_core/trunk/rtl/verilog
 define_design_lib WORK -path ./work
 
 analyze -f verilog [list aes_cipher_top.v aes_key_expand_128.v aes_rcon.v aes_sbox.v timescale.v] 
-elaborate aes 
+elaborate aes_cipher_top 
 link
 write hier f ddc output unmapped/aes.ddc
 read_ddc unmapped/aes.ddc
-create_clock -period 2 CK
-set_clock_latency 0.4 [get_clocks CK]
-set_clock_uncertainty  0.05  [get_port CK]
-set_clock_transition 0.1 [get_clocks CK]
+create_clock -period 10 clk
+set_clock_latency 0.4 [get_clocks clk]
+set_clock_uncertainty  0.05  [get_port clk]
+set_clock_transition 0.1 [get_clocks clk]
 check_design
 check_timing
 report_timing_requirements
@@ -27,6 +27,7 @@ report_power > reports/power.txt
 report_cell > reports/cell.txt
 report_path_group > reports/path_group.txt
 
+write_sdc work/aes.sdc
 write -hier -f verilog -output work/aes.v
 write -hier -f ddc -output work/aes.ddc
 
